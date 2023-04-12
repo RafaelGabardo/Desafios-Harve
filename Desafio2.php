8b58b4a2454818b9f02be9e9bd7517cb752d1171
Rafael Gabardo <rggabardo@gmail.com>
2023-04-12 17:00:50 -0300

    Desafio2.php correção2

--- Desafio2.php
+++ Desafio2.php
@@ -8,8 +8,8 @@
     $leiaTenis = ['t', 'e', 'n', 'i', 's',];
     $substituaTenis = ['p', 'o', 'l', 'a', 'r',];
 
-    $novoSobrenome = str_replace($leiaTenis, $substituaTenis, $sobrenome);
-    $novoNome = str_replace($leiaPolar, $substituaPolar, $nome);
+    $novoSobrenome = str_replace($leiaPolar, $substituaPolar, $sobrenome);
+    $novoNome = str_replace($leiaTenis, $substituaTenis, $nome);
     
     echo $novoNome . '<br>';
     echo $novoSobrenome;
