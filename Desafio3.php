4561e78f88ef899e7df0f23ef29033ccbc108e02
Rafael Gabardo <rggabardo@gmail.com>
2023-04-20 21:00:47 -0300

    Correção Desafio3.php

--- Desafio3.php
+++ Desafio3.php
@@ -1,23 +1,29 @@
 <?php
+    // Declarando as variáveis
     $nome = 'Rafael';
     $nota1 = 9;
-    $nota2 = 4;
+    $nota2 = 10;
     $nota3 = 8;
     $numeroDeNotas = 3;
-    $faltas = 30;
+    $aulasFrequentadas = 19;
+    $aulas = 90;
 
-    $float = ($nota1 + $nota2 + $nota3) / $numeroDeNotas;
+    // Fazendo o cálculo da nota
+    $notaFinal = ($nota1 + $nota2 + $nota3) / $numeroDeNotas;
+    $notaArredondada = round($notaFinal, 0);
 
-    if($faltas === 0) {
+    // Fazendo o cálculo da frequência
+    if($aulasFrequentadas === 0) {
         $frequencia = 0;
     } else {
-        $frequencia = ($faltas/90) * 100;
+        $frequencia = ($aulasFrequentadas/$aulas) * 100;
+        $frequencia = round($frequencia, 0);
     }
 
-
-    if($float < 8 || $frequencia < 20) {
-        echo 'Aluno '. $nome . ' obteve nota ' . number_format($float, 1, ',', ' ') . ' e um total de ' . number_format($frequencia, 1, ',', ' ') . '% de faltas e por isso está REPROVADO!';
+    // Finalizando o serviço
+    if($notaArredondada < 8 || $frequencia < 20) {
+        echo 'Aluno '. $nome . ' obteve nota ' . $notaArredondada . ' e um total de ' . $frequencia . '% de faltas e por isso está APROVADO!';
     } else {
-        echo 'Aluno '. $nome . ' obteve nota ' . number_format($float, 1, ',', ' ') . ' e um total de ' . number_format($frequencia, 1, ',', ' ') . '% de faltas e por isso está APROVADO!';
+        echo 'Aluno '. $nome . ' obteve nota ' . $notaArredondada . ' e um total de ' . $frequencia . '% de faltas e por isso está REPROVADO!';
     }
 ?>
