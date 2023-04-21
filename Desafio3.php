b74769b624850b7d45ec24f4766bcdd1e26bd51d
Rafael Gabardo <rggabardo@gmail.com>
2023-04-20 21:30:27 -0300

    Correção 2 Desafio3.php

--- Desafio3.php
+++ Desafio3.php
@@ -8,22 +8,28 @@
     $aulasFrequentadas = 19;
     $aulas = 90;
 
-    // Fazendo o cálculo da nota
-    $notaFinal = ($nota1 + $nota2 + $nota3) / $numeroDeNotas;
-    $notaArredondada = round($notaFinal, 0);
+    function calculo($nota1, $nota2, $nota3, $numeroDeNotas, $aulasFrequentadas, $aulas, $nome) {
+        // Fazendo cálculo da nota
+        $notaFinal = ($nota1 + $nota2 + $nota3) / $numeroDeNotas;
+        $notaArredondada = round($notaFinal, 0);
 
-    // Fazendo o cálculo da frequência
-    if($aulasFrequentadas === 0) {
-        $frequencia = 0;
-    } else {
-        $frequencia = ($aulasFrequentadas/$aulas) * 100;
-        $frequencia = round($frequencia, 0);
+        // Fazendo cálculo da frequência
+        if($aulasFrequentadas === 0) {
+            $frequencia = 0;
+        } else {
+            $frequencia = ($aulasFrequentadas/$aulas) * 100;
+            $frequencia = round($frequencia, 0);
+        }
+
+        // Finalizando o serviço
+        if($notaArredondada < 8 || $frequencia < 20) {
+            echo 'Aluno '. $nome . ' obteve nota ' . $notaArredondada . ' e um total de ' . $frequencia . '% de faltas e por isso está APROVADO!';
+        } else {
+            echo 'Aluno '. $nome . ' obteve nota ' . $notaArredondada . ' e um total de ' . $frequencia . '% de faltas e por isso está REPROVADO!';
+        }
+
+        return;
     }
 
-    // Finalizando o serviço
-    if($notaArredondada < 8 || $frequencia < 20) {
-        echo 'Aluno '. $nome . ' obteve nota ' . $notaArredondada . ' e um total de ' . $frequencia . '% de faltas e por isso está APROVADO!';
-    } else {
-        echo 'Aluno '. $nome . ' obteve nota ' . $notaArredondada . ' e um total de ' . $frequencia . '% de faltas e por isso está REPROVADO!';
-    }
+    echo calculo($nota1, $nota2, $nota3, $numeroDeNotas, $aulasFrequentadas, $aulas, $nome);
 ?>
