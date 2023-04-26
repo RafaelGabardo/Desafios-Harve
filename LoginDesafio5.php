c5e6130ad4c04f8ee13b9b43248b7eaefe6c90f3
Rafael Gabardo <rggabardo@gmail.com>
2023-04-25 21:30:02 -0300

    Deixando o código mais legível

--- LoginDesafio5.php
+++ LoginDesafio5.php
@@ -1,10 +1,13 @@
 <?php
+    // Começando o código
     if(isset($_POST['submit'])) {
+        //Declarando variáveis
         $fullname = $_POST['fullname'];
         $birthdate = $_POST['birthdate'];
         $email = $_POST['email'];
         $pass = $_POST['pass'];
 
+        // Fazendo o tal do "query"
         $sql = "
             SELECT
                 *
@@ -14,8 +17,10 @@
                 name = :name AND birthdate = :birthdate AND email = :email AND password = :pass
         ";
 
+        // Garantindo/Assegurando que esttará tudo correto
         $ensure = [$fullname=>':name', $birthdate=>':birthdate', $email=>':email', $pass=>':pass'];
 
+        // Terminando o serviço
         if($sql == $ensure) {
             require('LoginDesafio5.php');
         } else {
